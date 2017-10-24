b44a59d1fc6d5b27621716c33385ba78410584dc
yangtze736 <jiangzheahu@126.com>
2017-10-25 01:07:09 +0800

    add two numbers

---
+++ add_two_numbers.cc
@@ -0,0 +1,66 @@
+/*
+You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8
+
+分析
+	给定两个链表，返回其相加的结果。注意链表是倒序的，即第一位是个位。
+	342 + 465 = 807
+ */
+
+#include<iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {  }
+};
+
+class Solution {
+public:
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+		ListNode *res = new ListNode(0);
+		ListNode *cur = NULL;
+		int carry = 0;
+		while (l1 || l2) {
+			int n1 = l1 ? l1->val : 0;
+			int n2 = l2 ? l2->val : 0;
+			int sum = n1 + n2 + carry;
+			carry = sum / 10;
+			cur->val = sum % 10;
+			cur->next = new ListNode(0);
+			cur = cur->next;
+			if (l1) l1 = l1->next;
+			if (l2) l2 = l2->next;
+		}
+		//最高位进位
+		if (carry) cur->next = new ListNode(1);
+		return res;
+	}
+};
+
+int main()
+{
+	ListNode *p1 = new ListNode(2);
+	p1->next = new ListNode(4);
+	p1->next->next = new ListNode(3);
+
+	ListNode *p2 = new ListNode(5);
+	p2->next = new ListNode(6);
+	p2->next->next = new ListNode(4);
+
+	Solution solu;
+	ListNode *ret = solu.addTwoNumbers(p1, p2);
+
+	ListNode *p = ret;
+	if(p->next != NULL) {
+		cout << p->val << " ";
+		p = p->next;
+	}
+	cout << endl;
+
+	// delete p1 p2 ret
+	// todo
+	return 0;
+}
