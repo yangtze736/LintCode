a41046514550e5e9386c1d67d967149f385e7d81
yangtze736 <jiangzheahu@126.com>
2017-10-24 16:18:51 +0800

    Create set_matrox_zeros.cc

---
+++ set_matrox_zeros.cc
@@ -0,0 +1,63 @@
+/*
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+
+Follow up:
+Did you use extra space?
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+
+感觉像是泡泡堂里的炸弹，横竖扩展
+
+分析
+- 先扫描第一行第一列，如果有0，则将各自的flag设置为true
+- 扫描除去第一行第一列的整个数组，如果有0，则将对应的第一行和第一列的数字赋0
+- 扫描除去第一行第一列的整个数组，如果对应的第一行和第一列的数字有一个为0，则将当前值赋0
+- 最后根据第一行第一列的flag来更新第一行第一列
+*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+ void setZeroes(vector<vector<int>> &matrix) {
+  if(matrix.empty() || matrix[0].empty()) return;
+  int m = matrix.size(), n = matrix[0].size();
+  
+  bool rowflag = false, colflag = false;
+  for( int i = 0; i < m; i++ ) {
+      if(matrix[i][0] == 0) { colflag = true; break; }
+  }
+  for( int i = 0; i < n; i++ ) {
+      if(matrix[0][i] == 0) { rowflag = true; break; }
+  }
+  
+  for( int i = 1; i < m; i++ ) {
+    for( int j = 1; j < n; j++ ) {
+      if( matrix[i][j] == 0 ) { matrix[0][j] = 0; matrix[i][0] = 0; }
+    }
+  }
+  
+  for( int i = 1; i < m; i++ ) {
+    for( int j = 1; j < n; j++ ) {
+      if( matrix[0][j] == 0 || matrix[i][0] == 0 ) { matrix[i][j] = 0; }
+    }
+  }
+    
+  if(rowflag) {
+   for( int i = 0; i < n; i++ ) matrix[0][i] = 0;
+  }
+  if(colflag) {
+   for( int i = 0; i < m; i++ ) matrix[i][0] = 0;
+  }
+  
+ }
+};
+
+int main()
+{
+  return 0;
+}
