4ad4ea620ea33d7320cc2c033978de2fc366b070
yangtze736 <jiangzheahu@126.com>
2017-10-27 23:16:40 +0800

    modify file indent

--- merge_sorted_array.cc
+++ merge_sorted_array.cc
@@ -5,10 +5,10 @@ Note:
 You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are m andn respectively.
 
 分析
-	算法思想是：由于合并后A数组的大小必定是m+n，所以从最后面开始往前
-	赋值，先比较A和B中最后一个元素的大小，把较大的那个插入到m+n-1的
-	位置上，再依次向前推。如果A循环完了，B中还有元素没加入A素没加入A，
-	直接把B中所有的元素覆盖到A剩下的位置。
+    算法思想是：由于合并后A数组的大小必定是m+n，所以从最后面开始往前
+    赋值，先比较A和B中最后一个元素的大小，把较大的那个插入到m+n-1的
+    位置上，再依次向前推。如果A循环完了，B中还有元素没加入A素没加入A，
+    直接把B中所有的元素覆盖到A剩下的位置。
  */
 
 #include<vector>
@@ -18,13 +18,13 @@ using namespace std;
 
 class Solution {
 public:
-	void merge(int A[], int m, int B[], int n) {
-		int count = m + n - 1;
-		m--;n--;
+    void merge(int A[], int m, int B[], int n) {
+        int count = m + n - 1;
+        m--;n--;
 
-		while( m >= 0 && n >= 0 ) A[count--] = A[m] > B[n] ? A[m--] : B[n--];
-		while( n >= 0 ) A[count--] = B[n--];
-	}
+        while( m >= 0 && n >= 0 ) A[count--] = A[m] > B[n] ? A[m--] : B[n--];
+        while( n >= 0 ) A[count--] = B[n--];
+    }
 };
 
 void printArray(int A[], int n) {
@@ -37,7 +37,7 @@ void printArray(int A[], int n) {
 
 int main()
 {
-		Solution solu;
+    Solution solu;
 
     int a[]={2,4,6,8,10,0,0,0};
     int b[]={1,3,5};
