d972005c962d1c0c2be287131249f7c2dcf84001
yangtze736 <jiangzheahu@126.com>
2017-10-27 23:18:32 +0800

    modify file indent

--- merge_sorted_array.cc
+++ merge_sorted_array.cc
@@ -2,7 +2,9 @@
 Given two sorted integer arrays A and B, merge B into A as one sorted array.
 
 Note:
-You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are m andn respectively.
+You may assume that A has enough space (size that is greater or equal to m + n)
+to hold additional elements from B. The number of elements initialized in A 
+and B are m andn respectively.
 
 分析
     算法思想是：由于合并后A数组的大小必定是m+n，所以从最后面开始往前
